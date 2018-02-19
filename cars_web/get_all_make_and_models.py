a421555558977caad1b39b54d028b63332f386ba
nishafnaeem7 <besf14a531@pucit.edu.pk>
2018-02-19 23:58:43 +0500

    Changed

--- cars_web/get_all_make_and_models.py
+++ cars_web/get_all_make_and_models.py
@@ -13,8 +13,9 @@ from cars_web.models import CarModels
 class PopulateSelectingDatabase:
     def __init__(self):
         display = Display(visible=0, size=(1500,800))
-        display.start()
-        self.driver = webdriver.Chrome('/root/cars_web/cars_web/chromedriver')#, chrome_options=self.get_chrome_options())
+        #display.start()
+        print(os.getcwd() + '/cars_web/chromedriver')
+        self.driver = webdriver.Chrome(os.getcwd() + '/cars_web/chromedriver')#, chrome_options=self.get_chrome_options())
         self.autotrader = 'https://www.autotrader.com/'
         self.carsdotcom = 'https://www.cars.com/'
         self.carsforsale = 'https://www.carsforsale.com'
