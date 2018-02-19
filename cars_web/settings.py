3bb6ee07eaa1483918fab1843f1ed4bc8f8f1d5f
nishafnaeem7 <besf14a531@pucit.edu.pk>
2018-02-19 20:08:18 +0500

    Changed

--- cars_web/settings.py
+++ cars_web/settings.py
@@ -78,6 +78,13 @@ WSGI_APPLICATION = 'cars_web.wsgi.application'
 
 DATABASES = {
      'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+'''
+DATABASES = {
+     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'cars',
         'USER': 'postgres',
@@ -85,6 +92,7 @@ DATABASES = {
         'HOST': '127.0.0.1',
     }
 }
+'''
 
 
 # Internationalization
