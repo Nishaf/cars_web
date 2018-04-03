73eb443dae384bd89d25d7965cf0872d7a2c0f29
nishafnaeem7 <besf14a531@pucit.edu.pk>
2018-04-03 16:38:50 +0500

    Car Years Implemented.

--- cars_scrapers/get_models.py
+++ cars_scrapers/get_models.py
@@ -16,7 +16,7 @@ class PopulateSelectingDatabase:
     def __init__(self):
         display = Display(visible=0, size=(1500,800))
         display.start()
-        self.driver = webdriver.Chrome(BASE_DIR + "/chromedriver")#, chrome_options=self.get_chrome_options())
+        self.driver = webdriver.Chrome(BASE_DIR + "/chromedriver_linux")#, chrome_options=self.get_chrome_options())
         self.autotrader = 'https://www.autotrader.com/'
         self.carsdotcom = 'https://www.cars.com/'
         self.carsforsale = 'https://www.carsforsale.com'
